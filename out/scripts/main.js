328b010aa05f505c1bd8cbddfa8966a4244448c7
Peter Andringa <peter9221@gmail.com>
2014-10-11 02:15:16 -0400

    working registration

--- out/scripts/main.js
+++ out/scripts/main.js
@@ -1,91 +1,227 @@
 jQuery(function($){
 
+	/********************************************/
+	/*											*/
+	/* Setup         				  		    */
+	/*											*/
+	/********************************************/
+
+	// Currently a test key
+	Stripe.setPublishableKey('pk_test_bVNI8WnLVJlwNySLMliWPRjW');
+
+	$.postJSON = function(url, data, callback) {
+	    return jQuery.ajax({
+	        'type': 'POST',
+	        'url': url,
+	        'contentType': 'application/json',
+	        'data': JSON.stringify(data),
+	        'dataType': 'json',
+	        'success': callback
+	    });
+	};
+
+	var valid_card = ['visa', 'mastercard', 'amex', 'dinersclub', 'jcb'];
+
+	/********************************************/
+	/*											*/
+	/* Modal Controls         		  		    */
+	/*											*/
+	/********************************************/
+
 	var registerButton = $('.js-register'),
 		modal = $( '#registerModal' ),
-		submit = $( '#registerModal .js-modal-close' ),
+		close = $( '.js-modal-close' ),
+		submit = $( '.js-submit' ),
 		overlay = $( '.modal-overlay' );
 
-	// Modal Opening/Closing
 	function closeModal(){
 		modal.removeClass('show' );
 	}
 	registerButton.on( 'click', function( event ) {
 		modal.addClass('show')
-
+		$('input.name').focus()
 		overlay.unbind('click', closeModal );
 		overlay.on( 'click', closeModal );
 	});
-	submit.on( 'click', function( ev ) {
+	close.on( 'click', function( ev ) {
 		ev.stopPropagation();
 		closeModal();
 	});
 	$('.show-discount').on('click', function(){
-		$('input.discount').parent().removeClass('hidden')
+		var parent = $('input.discount').parent()
+		if( parent.hasClass('hidden') ){
+			parent.removeClass('hidden');
+			$('input.discount').focus()
+			$(this).text('Hide discount field');
+		}else{
+			parent.addClass('hidden');
+			$(this).text('Have a discount code?');
+		}
 	});
 
-	// Form Validation
+	/********************************************/
+	/*											*/
+	/* Form Validation         		  		    */
+	/*											*/
+	/********************************************/
+
 	$('input.cc-num').payment('formatCardNumber');
 	$('input.cc-exp').payment('formatCardExpiry');
 	$('input.cc-cvc').payment('formatCardCVC');
 	$('[data-numeric]').payment('restrictNumeric');
 
+	$('input:not(.cc-num)').typeWatch({
+		event: 'typingDone',
+		wait: 750
+	});
+
 	var cardType;
-	var nameOk = emailOk = numOk = cvcOk = expOk = true;
+	var nameOk = emailOk = numOk = cvcOk = expOk = false;
 	function checkSubmit(){
-		if(nameOk && emailOk && numOk && cvcOk && expOk){
-			submit.removeClass('disabled')
-		}else{
-			submit.addClass('disabled')
-		}
+		if(nameOk && emailOk && numOk && cvcOk && expOk)
+			submit.removeClass('disabled');
+		else
+			submit.addClass('disabled');
+		
+		return nameOk && emailOk && numOk && cvcOk && expOk;
 	}
 	$('input.cc-num').on('keyup', function(evt){
-		var newType = $.payment.cardType( parseInt( $(this).val() ) )
-		if( newType && newType != cardType ){
-			cardType = newType
-			console.log(cardType);
+		$(this).parent().removeClass('empty error');
+
+		var newType = $.payment.cardType( parseInt( $(this).val() ) );
+		if( newType != cardType){
+			if( cardType )
+				$(this).siblings('.card').removeClass(cardType);	
+			if( newType ){
+				if(valid_card.indexOf(newType) != -1){
+					$(this).siblings('.card').addClass(newType);
+					cardType = newType
+				}else{
+					cardType = null;
+				}
+			}
 		}
 	});
 
-	$('input.name').on('blur', function(evt){
-		nameOk = $(this).val().match(/^[a-zA-Z\\s]+ /i)
-		checkSubmit()
+	$('input.name').on('typingDone', function(evt){
+		$(this).parent().removeClass('empty');
+		nameOk = $(this).val().match(/^[a-zA-Z\\s]+ /i);
+		checkSubmit();
 		if(nameOk)
 			$(this).parent().removeClass('error');
 		else
 			$(this).parent().addClass('error');
 	});
-	$('input.email').on('blur', function(evt){
-		emailOk = $(this).val().match(/\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/i)
-		checkSubmit()
+	$('input.email').on('typingDone', function(evt){
+		$(this).parent().removeClass('empty');
+		emailOk = $(this).val().match(/\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/i);
+		checkSubmit();
 		if(emailOk)
 			$(this).parent().removeClass('error');
 		else
 			$(this).parent().addClass('error');
 	});
 	$('input.cc-num').on('blur', function(evt){
-		numOk = $.payment.validateCardNumber( $(this).val() )
-		checkSubmit()
+		console.log(cardType)
+		if(!cardType || cardType == undefined){
+			var card = $(this).siblings('.card').addClass('pe-7s-check');
+			cardType = 'pe-7s-check';
+		}
+		$(this).parent().removeClass('empty');
+		numOk = $.payment.validateCardNumber( $(this).val() );
+		checkSubmit();
 		if( numOk )
 			$(this).parent().removeClass('error');
 		else
 			$(this).parent().addClass('error');
 	});
-	$('input.cc-cvc').on('blur', function(evt){
-		cvcOk = $.payment.validateCardCVC( $(this).val(), cardType )
-		checkSubmit()
+	$('input.cc-cvc').on('typingDone', function(evt){
+		$(this).parent().removeClass('empty');
+		cvcOk = $.payment.validateCardCVC( $(this).val(), cardType );
+		checkSubmit();
 		if( cvcOk )
 			$(this).parent().removeClass('error');
 		else
 			$(this).parent().addClass('error');
 	});
-	$('input.cc-exp').on('blur', function(evt){
-		var value = $(this).payment('cardExpiryVal')
-		expOk = $.payment.validateCardExpiry( value.month, value.year )
-		checkSubmit()
+	$('input.cc-exp').on('typingDone', function(evt){
+		$(this).parent().removeClass('empty');
+		var value = $(this).payment('cardExpiryVal');
+		expOk = $.payment.validateCardExpiry( value.month, value.year );
+		checkSubmit();
 		if( expOk )
 			$(this).parent().removeClass('error');
 		else
 			$(this).parent().addClass('error');
 	});
+	$('input.discount').on('typingDone', function(evt){
+		if( $(this).hasClass('hidden') ){
+			$(this).val();
+			$(this).parent().addClass('empty');
+			$(this).parent().removeClass('error');
+			return;
+		}
+		$(this).parent().removeClass('empty');
+		
+		// Post request to check discount
+			// 	$(this).parent().removeClass('error');
+			// else
+			// 	$(this).parent().addClass('error');
+	});
+
+
+	/********************************************/
+	/*											*/
+	/* Form Submission         		  		    */
+	/*											*/
+	/********************************************/
+
+	$('.js-submit').on('click', function(ev){
+		ev.preventDefault();
+
+		$('input.name').trigger('typingDone');
+		$('input.email').trigger('typingDone');
+		$('input.cc-num').trigger('blur');
+		$('input.cc-cvc').trigger('typingDone');
+		$('input.cc-exp').trigger('typingDone');
+		if( ! $('input.discount').hasClass('hidden') )
+			$('input.discount').trigger('typingDone');
+
+		var expDate = $('input.cc-exp').payment('cardExpiryVal');
+
+		var stripeData = {
+		  number: $('input.cc-num').val(),
+		  cvc: $('input.cc-cvc').val(),
+		  exp_month: expDate.month,
+		  exp_year: expDate.year
+		}
+		Stripe.card.createToken(stripeData, function(status, response){
+			if(response.error){
+				console.log("Error!", response.error);
+			}else{
+				console.log("Card", response.card)
+				var data = {
+					user: {
+						name: $('input.name').val(),
+						email: $('input.email').val(), 
+					},
+					stripe_token: response.id,
+					discount: $('input.discount').val() || false
+				}
+
+				$.postJSON('http://api.gopilot.org/events/'+PILOT_EVENT_ID+'/register', data, function(data){
+					$('.js-submit').addClass('success');
+					$('.js-submit').html('<i class="pe-7s-check"></i>');
+					setTimeout(function(){
+						modal.removeClass('show') // Temp, until we show a page after the request succeeds
+					}, 1500)
+				});
+				$('js-submit').html('<i class="pe-7s-config spin"></i>');
+			}
+		});
+		$('.js-submit').addClass('has-icon')
+		$('.js-submit').html('<i class="pe-7s-config spin"></i>');
+	});
+
 });
 
