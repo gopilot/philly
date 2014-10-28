69dffb7e71e351e2bf5fd2d5a23d38a560818b02
Mayank Jain <mjmayank@gmail.com>
2014-10-27 20:15:55 -0500

    philly site

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ function runServer(port) {
 
 
 gulp.task('deploy', ['stylus', 'html', 'static', 'scripts'], function () {
-    var remote = "https://github.com/gopilot/miami.git";
+    var remote = "https://github.com/gopilot/philly.git";
 
     return gulp.src("./out/**/*")
         .pipe( deploy( remote ) );
